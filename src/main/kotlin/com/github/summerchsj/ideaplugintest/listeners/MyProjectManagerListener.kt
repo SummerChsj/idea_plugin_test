7923114b4cf2d2c8a3a2354fd35070ab43585765
GitHub Action <action@github.com>
2022-08-05 01:02:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/summerchsj/ideaplugintest/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/summerchsj/ideaplugintest/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.summerchsj.ideaplugintest.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.summerchsj.ideaplugintest.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/summerchsj/ideaplugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/summerchsj/ideaplugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.summerchsj.ideaplugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.summerchsj.ideaplugintest.MyBundle
 
 class MyApplicationService {
 
